29c52c7f670f2ca989aef6b886128174d8a85c41
Jvargas <juan20028c@gmail.com>
2025-04-16 20:56:53 -0500

    guardo aqui porque ya voy a descanasar del estudio, pero aun me falta ver las peticiones http

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+<template>
+    <div name="header" class="d-flex justify-content-center align-items-center py-2 px-4 bg-dark text-white fixed-top">
+        <h2 class="m-0">La temperatura actual es:</h2>
+    </div>  
+</template>
+<style scoped>
+
+</style>
--- src/views/mainview.vue
+++ src/views/mainview.vue
@@ -1,8 +1,10 @@
 <script setup>
     import SideBar from '@/components/SideBar.vue';
+    import Header from '@/components/Header.vue';
 </script>
 <template>
-<div class="d-flex">
+    <Header></Header>
+<div class="d-flex mt-30px">
     <SideBar></SideBar>
     <div class="flex-grow-1 p-3">
         <router-view></router-view>
@@ -11,5 +13,7 @@
 
 </template>
 <style scoped>
-
+    .mt-30px{
+        margin-top: 30px;
+    }
 </style>
